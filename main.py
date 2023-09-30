5e99038203accc1545acf766cd79e01bec62e6bf
Asquator <vistalus@protonmail.com>
2023-09-30 16:57:17 +0300

    added SVM poly and naive Bayes

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from models import *
 from fitter import *
 from dataset import Dataset
 
-models = {'logistic': log_reg, 'svm': svclf}
+models = {'logistic': log_reg, 'SVM Gaussian': svm_gaussian, 'SVM polynomial': svm_poly, 'naive Bayes' : nbg}
 
 if __name__ == '__main__':
     ds = Dataset()
     X, y = ds.X, ds.y
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.12)
 
     for name, model in models.items():
         print("Algorithm: " + name)
--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
 
 log_reg = LogisticRegression(max_iter=1000, multi_class='ovr')
-svclf = svm.SVC(kernel="rbf", C=2.3)
+svm_gaussian = svm.SVC(kernel="rbf", C=2.3)
+svm_poly = svm.SVC(kernel="poly", degree=4, C=0.8)
+nbg = GaussianNB()
